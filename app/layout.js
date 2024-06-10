fb1d7cf7ec9ff488b81edc1b6d5d2a512a1e2ea0
Sunti <saknsaza87@gmail.com>
2024-06-10 12:49:26 +0700

    login

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,10 @@
-"use client"
 import { Inter } from "next/font/google";
-
+import "@/app/style/globals.css"
 const inter = Inter({ subsets: ["latin"] });
-
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="no-scroll-login">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -4,14 +4,24 @@ import Image from "next/image";
 
 export default function Counter() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hello next</div>
-      <Image
-        src="/imgs/messageImage_1715250896427.jpg"
-        width={100}
-        height={300}
-        alt="Picture of the author"
-      />
+    <main className="flex h-screen w-screen">
+      <section className=" h-full w-1/3 relative">
+        <div className="w-[70%] absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+          <Image src={"/imgs/subway-logo-login.png"} width={20000} height={1000} />
+        </div>
+        <div className="w-full h-full">
+          <Image src={"/imgs/login-bg-subway.png"} width={500} height={1000} style={{ width: "100%", height: "100%" }} />
+        </div>
+      </section>
+      <section className="bg-[#03741E] h-full w-2/3 relative">
+        <div className="w-[50%] absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white h-[530px] p-[40px] gap-[56px] rounded-[32px] ">
+          <div>
+            {/* <Image src={"/imgs/"} width={500} height={1000} style={{ width: "100%", height: "100%" }} /> */}
+          </div>
+          <div>login</div>
+
+        </div>
+      </section>
     </main>
   );
 }
